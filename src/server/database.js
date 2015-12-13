6f3f9c54f59d051b7c8f7db51e0689193074df06
Vu Nam <vunam281@gmail.com>
2015-12-13 10:21:31 +0000

    Completed algorithm to Database

--- src/server/database.js
+++ src/server/database.js
@@ -3,9 +3,25 @@ var mongoSchema =   mongoose.Schema;
 
 mongoose.connect('mongodb://localhost:27017/marketIntel');
 
+// Domain Model
 var domainSchema = new mongoSchema ({
     "_id" : Number,
     "name" : String
 });
 
-module.exports =  mongoose.model('domains', domainSchema);
+var domains = mongoose.model('domains', domainSchema);
+
+// Record Model
+var recordSchema = new mongoSchema ({
+    "_id" : Number,
+    "parent" : Number,
+    "date" : String,
+    "score" : String
+});
+
+var records = mongoose.model('records', domainSchema);
+
+module.exports = {
+	domains : domains,
+	records : records
+};
--- src/server/server.js
+++ src/server/server.js
@@ -1,9 +1,9 @@
 var express = require('express'),
-	 fs = require('fs');
+	 fs = require('fs'),
+	 db = require("./database"),
+ 	 _ = require('underscore');
 
 var app = express();
-
-var db = require("./database");
 
 /*
 
@@ -21,6 +21,20 @@ app.get('/json', function(req,res){
 	var text = fs.readFileSync('./marketintel.json').toString();
    res.setHeader('Content-Type', 'application/json');
 	res.header("Access-Control-Allow-Origin", "*");
+
+	var data = JSON.parse(text).marketIntel;
+
+	_.each(data, function(dVal, dKey) {
+		var dQuery = { '_id': dKey, 'name': dVal.domain }
+		db.domains.findOneAndUpdate(dQuery, dQuery, {upsert:true, 'new': true}, function () {});
+		
+		_.each(dVal.scores, function(rVal, rKey) {
+			var rQuery = { 'domain': dKey, 'date': rKey, 'score': rVal }
+			db.records.findOneAndUpdate(rQuery, rQuery, {upsert:true, 'new': true}, function () {});
+		})
+
+	});
+
 	res.status(200).send(text);
 
 });
