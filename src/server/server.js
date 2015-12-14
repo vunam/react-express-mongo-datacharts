43cf2e953b11d9e1992f06338560687a23cb7f2b
Vu Nam <vunam281@gmail.com>
2015-12-14 20:20:55 +0000

    Save any files before styling

--- src/server/server.js
+++ src/server/server.js
@@ -27,7 +27,7 @@ app.get('/get_domains', function(req,res){
 
 app.get('/get_records/:domain_id', function(req,res){
 	var domain_id = req.params.domain_id;
-	db.records.find({ domain: domain_id }).sort('date', -1).execFind(function(err,data){
+	db.records.find({ domain: domain_id }).sort({'date': 1}).exec(function(err,data){
 		var response = err ? {"error" : true,"message" : "Error fetching data"} : {"error" : false,"message" : data}; 
 
 		res.header("Access-Control-Allow-Origin", "*");
